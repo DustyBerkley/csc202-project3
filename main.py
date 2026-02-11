db4dedd44bc3ff2d134bb661543115ec95e9b640
Dustin Berkley <78814395+yellowww@users.noreply.github.com>
2026-02-11 12:45:07 -0800

    added cnt_freq function

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ import unittest
 import sys
 sys.setrecursionlimit(10**6)
 
+# when given a string, return a list of the frequencies of each character in the string
+def cnt_freq(text: str) -> List[int]:
+    counts = [0] * 256
+    for char in text:
+        idx = ord(char)
+        counts[idx] += 1
+    return counts
+
 
 
 class Tests(unittest.TestCase):
-    pass
+    def test_cnt_freq(self):
+        str_1 : str = "aaabccddddd"
+        str_2 : str = "aaeefad"
+        self.assertEqual(cnt_freq(str_1)[96:104], [0, 3, 1, 2, 5, 0, 0, 0])
+        self.assertEqual(cnt_freq(str_2)[96:104], [0, 3, 0, 0, 1, 2, 1, 0])
 
 
 if (__name__ == '__main__'):
